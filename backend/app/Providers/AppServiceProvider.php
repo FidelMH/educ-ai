b8246f66a42426485e302f5d61491b2c15ae6ff0
Nairod97425 <dorian.hoa97425@gmail.com>
2025-11-19 13:49:56 +0400

    correction pour la keys trop long

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use App\Models\User;
 use App\Observers\UserObserver;
 
@@ -22,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         User::observe(UserObserver::class); // ✅ Enregistrer l'observer
-
+        Schema::defaultStringLength(191);
     }
 }
